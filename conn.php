6777643b904da886d1756414a09761abf9395164
LHY-1 <61404892+LHY-1@users.noreply.github.com>
2022-01-24 01:00:02 +0800

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+$link = new mysqli('localhost','users','123456','users');
+mysqli_set_charset($link, 'utf8');
+if ($link->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+$ranking = "kano_rank";//排行榜表名
